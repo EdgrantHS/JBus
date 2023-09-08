07ae20f19f915557a6e5b7b3c61018654870fc70
EdgrantHS <edgrant.h@gmail.com>
2023-09-08 10:39:22 +0700

    CS01 - Nambahin Semua Kerangka Function

--- Jbus/edgrantJBusRD/Jbus.java
+++ Jbus/edgrantJBusRD/Jbus.java
@@ -9,27 +9,84 @@ package edgrantJBusRD;
  */
 public class Jbus
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jbus
-     */
-    public Jbus()
+    public static void main(String string[])
     {
-        // initialise instance variables
-        x = 0;
+        int x;
     }
-
+    
+    public static int getBusId()
+    {
+        return 0;
+    }
+    
+    public static String getBusName()
+    {
+        return "0";
+    }
+    
+    public static boolean isDiscount()
+    {
+        return true;
+    }
+    
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
+    public static float getDiscountPercentage(int a, int b)
     {
-        // put your code here
-        return x + y;
+        return 0;
     }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static int getOriginalPrice(int a, float persen)
+    {
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static float getAdminFeePercentage()
+    {
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static int getAdminFee(int a)
+    {
+        return 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static int getTotalPrice(int a, int b)
+    {
+        return 0;
+    }
+    /*
+    public Jbus()
+    {
+        // initialise instance variables
+        x = 0;
+    }*/
 }
