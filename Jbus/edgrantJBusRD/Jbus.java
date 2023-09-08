426c472bf1cdb56713b51df10eec0b28dac136f1
EdgrantHS <edgrant.h@gmail.com>
2023-09-08 11:19:52 +0700

    CS01 - sudah didebug dan ditambah komentar penjelasan menthod

--- Jbus/edgrantJBusRD/Jbus.java
+++ Jbus/edgrantJBusRD/Jbus.java
@@ -11,7 +11,7 @@ public class Jbus
 {
     public static void main(String string[])
     {
-        int x;
+        
     }
     
     /**
@@ -53,7 +53,7 @@ public class Jbus
      */
     public static float getDiscountPercentage(int berforeDiscount, int afterDiscount)
     {
-        if (afterDiscount <= berforeDiscount)
+        if (afterDiscount >= berforeDiscount)
         {
             return 0.0f;
         }
@@ -64,6 +64,13 @@ public class Jbus
         }
     }
     
+    /**
+     * getDiscountedPrice - menghitung harga akhir setelah didiskon
+     *
+     * @param  price  harga sebelum diskon
+     * @param  discountPercentage  persentasi diskon
+     * @return    harga akhir setelah didiskon
+     */
     public static int getDiscountedPrice(int price, float discountPercentage)
     {
         float discountedPrice;
@@ -78,6 +85,13 @@ public class Jbus
         }
     }
     
+    /**
+     * getOriginalPrice - menghitung harga awal sebelum didiskon
+     *
+     * @param  discountedPrice  harga setelah diskon
+     * @param  discountPercentage  persentasi diskon
+     * @return    harga awal sebelum didiskon
+     */
     public static int getOriginalPrice(int discountedPrice, float discountPercentage)
     {
         float originalPrice;
@@ -92,21 +106,38 @@ public class Jbus
         }
     }
     
+    /**
+     * getAdminFeePercentage - mendapatkan persen admin yaitu 0.05
+     *
+     * @return    0.05
+     */
     public static float getAdminFeePercentage()
     {
         return 0.05f;
     }
     
+    /**
+     * getAdminFee - mendapatkan harga admin dengan persentasi 0.05
+     *
+     * @return    harga admin yang akan dikenakan
+     */
     public static int getAdminFee(int price)
     {
         float adminFee = price * getAdminFeePercentage();
         return (int) adminFee; //!cek ini return bener atau gak
     }
     
+    /**
+     * getAdminFee - menghitung harga pembelian kursi dengan pengenaan harga admin
+     *
+     * @param  price  harga satu kursi
+     * @param  numberOfSeat  jumlah kursi yang ingin dibeli
+     * @return    harga pembelian kursi dengan pengenaan harga admin
+     */
     public static int getTotalPrice(int price, int numberOfSeat)
     {
         int priceWithoutAdmin = price * numberOfSeat;
-        int priceAfterAdmin = priceWithoutAdmin - getAdminFee(priceWithoutAdmin);
+        int priceAfterAdmin = priceWithoutAdmin + getAdminFee(priceWithoutAdmin);
         
         return priceAfterAdmin;
     }
