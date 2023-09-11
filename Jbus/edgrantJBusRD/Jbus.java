7d322cc0631724bce6e80ca84aebbf0f0993b2e1
EdgrantHS <edgrant.h@gmail.com>
2023-09-11 21:20:45 +0700

    CS01 - revert changes

--- Jbus/edgrantJBusRD/Jbus.java
+++ Jbus/edgrantJBusRD/Jbus.java
@@ -11,7 +11,11 @@ public class Jbus
 {
     public static void main(String string[])
     {
+<<<<<<< HEAD
         System.out.println(getDiscountPercentage(100, 90));
+=======
+        System.out.println(getDiscountPercentage(1000,900));
+>>>>>>> parent of 62646ce (CS01 - telat, remove command di main)
     }
     
     /**
