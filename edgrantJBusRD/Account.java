797713662dcb7ca31a03e987e62fcb404c8815f5
EdgrantHS <edgrant.h@gmail.com>
2023-09-18 19:50:44 +0700

    PT02 - semua class selesai, belum didebbug)

---
+++ edgrantJBusRD/Account.java
@@ -0,0 +1,16 @@
+package edgrantJBusRD;
+
+public class Account
+{
+    // instance variables - replace the example below with your own
+    public String email, name, password;
+
+    /**
+     * Constructor for objects of class Account
+     */
+    public Account()
+    {
+        // initialise instance variables
+    }
+
+}
---
+++ edgrantJBusRD/Voucher.java
@@ -0,0 +1,68 @@
+package edgrantJBusRD;
+
+
+/**
+ * Write a description of class Voucher here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Voucher
+{
+    // instance variables - replace the example below with your own
+    public String name;
+    private boolean used;
+    public double minimum, cut;
+    public int code;
+    public Type type;
+    
+    public static void main(){
+        Voucher v = new Voucher("h", 3, Type.REBATE, 3000, 5000);
+        Price p = new Price(10000);
+        
+        System.out.println(v.apply(p));
+    }
+    
+    public Voucher(String name, int code, Type type, double minimum, double cut)
+    {
+        this.name = name;
+        this.code = code;
+        this.type = type;
+        this.minimum = minimum;
+        this.cut = cut;
+        this.used = false;
+    }
+    
+    public double apply(Price price)
+    {
+        double appliedPrice = -1;
+        
+        used = true;
+        if (this.type == Type.REBATE){
+            appliedPrice = price.price - cut;
+        }
+        
+        if (this.type == Type.DISCOUNT){
+            appliedPrice = price.price - (cut/100 * price.price);
+        }
+        
+        return appliedPrice;
+    }
+    
+    public boolean isUsed()
+    {
+        // put your code here
+        return this.used;
+    }
+    
+    public boolean canApply(Price price)
+    {
+        if (price.price > this.minimum){
+            if (!used){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
